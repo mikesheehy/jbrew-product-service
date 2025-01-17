4202f51a8b8570908d19aa28fd3841ec4120ed5e
Mike Sheehy <sheehy@outlook.com>
2025-01-16 23:52:48 -0500

    Reduced mongodb error to 500

--- src/main/java/com/mikesheehy/jbrew_product/repository/ProductRepository.java
+++ src/main/java/com/mikesheehy/jbrew_product/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.mikesheehy.jbrew_product.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import com.mikesheehy.jbrew_product.model.Product;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product,String>{
 
 }
